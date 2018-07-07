58355f42800f221758ee36ef119c46b43320db04
Dennis Brown <vilesyn@gmail.com>
2018-07-07 15:19:59 -0500

    setup basic remove/disable actions

--- app/components/actions.js
+++ app/components/actions.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Button from './visual/button'
 
-export default class Actions extends Component {
-  render() {
-    return (
-      <div className="action-container">
-        Actions
-        <div className="actions">
-          <Button title="copy selected settings folder" name="Copy Folder" />
-          <Button title="disable settings in selected settings folder" name="Disable Folder" />
-          <Button title="save the selected settings folder in a separate file" name="Save Folder" />
-          <Button title="make a backup of the selected settings folder" name="Backup Folder" />
-          <Button title="remove the selected settings folder" name="Remove Folder" />
-          <div>Script Actions</div>
-          <Button title="copy scripts from the selected settings folder to another" name="Copy Scripts" />
-          <Button title="remove scripts from the selected settings folder" name="Remove Scripts"/>
-        </div>
-      </div>
-    )
-  }
-}
+const Actions = ({selected, folders, copy, enable, disable, remove}) => (
+  <div className="action-container">
+    <div className="actions">
+      {selected.match(/^\d{1}\.\d{2}/) ?
+        <Fragment>
+          <Button title="copy selected settings folder" name="Copy Settings" action={() => copy(selected)} />
+          {selected.match(/-old/) === null ?
+            <Fragment>
+              {folders.indexOf(`${selected}-old`) === -1 ?
+                <Button title="disable settings folder" name="Disable Folder" action={() => disable(selected)} />
+              :
+                <div>Only one Disabled Folder per Version</div>
+              }
+            </Fragment>
+          :
+            <Fragment>
+              <Button title="enable settings folder" name="Enable Folder" action={() => enable(selected)} />
+            </Fragment>
+          }
+          <Button title="remove the selected settings folder" name="Remove Folder" action={() => remove(selected)} />
+        </Fragment>
+      : <div>Select Folder -></div>}
+    </div>
+  </div>
+)
+
+export default Actions
