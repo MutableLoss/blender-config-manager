e2f2e5ca4638f6435bf2e0c0a2bff759c695ea90
Dennis Brown <vilesyn@gmail.com>
2018-12-30 15:03:25 -0600

    remove task for now

--- electron/listeners/actions.js
+++ electron/listeners/actions.js
@@ -9,16 +9,16 @@ const windowCache = {}
 const dockNotificationCache = {}
 
 switch(process.platform) {
-  case 'darwin'
+  case 'darwin':
     blenderFolder = os.homedir() + '/Library/Application Support/Blender/'
     break
-  case 'win32'
+  case 'win32':
     blenderFolder = os.homedir() + '/AppData/Local/Blender Foundation/blender'
     break
-  case 'linux'
+  case 'linux':
     blenderFolder = os.homedir() + '/.config/blender/';
     break
-  case 'freebsd'
+  case 'freebsd':
     blenderFolder = os.homedir() + '/.config/blender/'
     break
 }
@@ -27,27 +27,24 @@ console.log(`OS: ${blenderFolder}`)
 // show folders in blender config folder
 ipcMain.on('show-folders', event =>
   fs.readdir(blenderFolder, (err, files) =>
-    err ? event.sender.send('show-folders-response', files.filter(file => file !== '.DS_Store')) : err))
+    !err ? event.sender.send('show-folders-response', files.filter(file => file !== '.DS_Store')) : err))
 
 // copy settings from one directory to another
 ipcMain.on('copy-settings', (event, from, to) =>
   fse.copy(`${blenderFolder}/${from}`, `${blenderFolder}/${to}`, err =>
-    err ? event.sender.send('copy-settings-response') : err))
+    !err ? event.sender.send('copy-settings-response') : err))
 
 // remove a blender config folder
 ipcMain.on('remove-folder', (event, folder) =>
-  task(res =>
-    fs.rmdir(folder, err =>
-      err ? res.resolve(event.sender.send('remove-folder-response')) : res.reject(err))))
+  fs.rmdir(folder, err =>
+    !err ? event.sender.send('remove-folder-response') : err))
 
 // disable a blender config folder by renaming to name.old
 ipcMain.on('disable-folder', (event, folder) =>
-  task(res =>
-    fs.rename(`${blenderFolder}/${folder}`, `${blenderFolder}/${folder}-old`, err => 
-      err ? res.resolve(event.sender.send('disable-folder-response')) : res.reject(err))))
+  fs.rename(`${blenderFolder}/${folder}`, `${blenderFolder}/${folder}-old`, err => 
+    !err ? event.sender.send('disable-folder-response') : err)))
 
 // enable a disabled folder
 ipcMain.on('enable-folder', (event, folder) =>
-  task(res =>
     fs.rename(`${blenderFolder}/${folder}`, `${blenderFolder}/${folder.replace(/.old/, '')}`, err =>
-      err ? res.resolve(event.sender.send('enable-folder-response')) : res.reject(err))))
+      !err ? event.sender.send('enable-folder-response') : err)))
