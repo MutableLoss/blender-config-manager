43a69e911fb3c4eb359f0c2adffd4651230bac14
Dennis Brown <vilesyn@gmail.com>
2018-12-30 14:59:04 -0600

    cleanup actions

--- electron/listeners/actions.js
+++ electron/listeners/actions.js
@@ -1,55 +1,53 @@
 const { ipcMain, dialog } = require('electron')
-const { task } = require('folktale/concurrency/task');
 const { exec } = require('child_process')
 const fs = require('fs')
 const fse = require('fs-extra')
 const os = require('os')
 
-let blenderPlatform;
-const windowCache = {};
-const dockNotificationCache = {};
+let blenderFolder
+const windowCache = {}
+const dockNotificationCache = {}
 
-blenderPlatform = switch(process.platform) {
-  case 'darwin':
-    os.homedir() + '/Library/Application Support/Blender/';
-    break;
-  case 'win32':
-    os.homedir() + '/AppData/Local/Blender Foundation/blender'
-    break;
-  case 'linux':
-    os.homedir() + '/.config/blender/';
-    break;
-  case 'freebsd':
-    os.homedir() + '/.config/blender/'
-    break;
+switch(process.platform) {
+  case 'darwin'
+    blenderFolder = os.homedir() + '/Library/Application Support/Blender/'
+    break
+  case 'win32'
+    blenderFolder = os.homedir() + '/AppData/Local/Blender Foundation/blender'
+    break
+  case 'linux'
+    blenderFolder = os.homedir() + '/.config/blender/';
+    break
+  case 'freebsd'
+    blenderFolder = os.homedir() + '/.config/blender/'
+    break
 }
-console.log(blenderPlatform)
+console.log(`OS: ${blenderFolder}`)
 
 // show folders in blender config folder
-ipcMain.on('show-folders', event => {
-  task((res) => 
-    fs.readdir(blenderPlatform, (err, files) => 
-      err ? res.resolve(event.sender.send('show-folders-response', files.filter(file => file !== '.DS_Store'))) : res.reject(err))));
+ipcMain.on('show-folders', event =>
+  fs.readdir(blenderFolder, (err, files) =>
+    err ? event.sender.send('show-folders-response', files.filter(file => file !== '.DS_Store')) : err))
 
 // copy settings from one directory to another
-ipcMain.on('copy-settings', (event, from, to) => {
-  task((res) =>
-    fse.copy(`${blenderPlatform}/${from}`, `${blenderPlatform}/${to}`, err => 
-      err ? res.resolve(event.sender.send('copy-settings-response')) : res.reject(err))));
+ipcMain.on('copy-settings', (event, from, to) =>
+  fse.copy(`${blenderFolder}/${from}`, `${blenderFolder}/${to}`, err =>
+    err ? event.sender.send('copy-settings-response') : err))
 
 // remove a blender config folder
-ipcMain.on('remove-folder', (event, folder) => {
-  task((res) =>
+ipcMain.on('remove-folder', (event, folder) =>
+  task(res =>
     fs.rmdir(folder, err =>
-      err ? res.resolve(event.sender.send('remove-folder-response')) : res.reject(err))));
+      err ? res.resolve(event.sender.send('remove-folder-response')) : res.reject(err))))
 
 // disable a blender config folder by renaming to name.old
-ipcMain.on('disable-folder', (event, folder) => {
-  task((res) =>
-    fs.rename(`${blenderPlatform}/${folder}`, `${blenderPlatform}/${folder}-old`, err => 
-      err ? res.resolve(event.sender.send('disable-folder-response')) : res.reject(err))));
+ipcMain.on('disable-folder', (event, folder) =>
+  task(res =>
+    fs.rename(`${blenderFolder}/${folder}`, `${blenderFolder}/${folder}-old`, err => 
+      err ? res.resolve(event.sender.send('disable-folder-response')) : res.reject(err))))
 
 // enable a disabled folder
-ipcMain.on('enable-folder', (event, folder) => {
-  fs.rename(`${blenderPlatform}/${folder}`, `${blenderPlatform}/${folder.replace(/.old/, '')}`, err => 
-    err ? res.resolve(event.sender.send('enable-folder-response')) : res.reject(err))));
+ipcMain.on('enable-folder', (event, folder) =>
+  task(res =>
+    fs.rename(`${blenderFolder}/${folder}`, `${blenderFolder}/${folder.replace(/.old/, '')}`, err =>
+      err ? res.resolve(event.sender.send('enable-folder-response')) : res.reject(err))))
