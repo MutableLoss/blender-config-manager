69ef105cfd86c1d9b937239584e7388baaf3148d
Dennis Brown <vilesyn@gmail.com>
2019-01-04 23:17:22 -0600

    add spacing

--- app/components/visual/ContainedButton.js
+++ app/components/visual/ContainedButton.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 
-export const ContainedButton = ({classes, title, action, name}) => (
+export const ContainedButton = ({ classes, title, action, name }) => (
   <Button 
     className={classes.button}
     color="primary"
