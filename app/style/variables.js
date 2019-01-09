b75001b8fe02ee557c6357a6fcb8a88d5e1dbed9
Dennis Brown <vilesyn@gmail.com>
2019-01-08 18:14:50 -0600

    add color variables

---
+++ app/style/variables.js
@@ -0,0 +1,12 @@
+export const systemOffwhite = '#e3e3e3'
+
+// THEME
+
+export const titleColor = 'hsl(207, 69%, 72%)'
+
+export const appBackground = 'hsl(0, 2%, 23%)'
+export const folderBackground = '#494444'
+
+export const buttonBack = 'hsl(0, 2%, 25%)'
+export const buttonBorder = '#272727'
+export const selectedBack = '#4484d8'
