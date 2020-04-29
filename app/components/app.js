2c64b683fad1d5afe3d6481391c1221cfa0ee25c
Dennis Brown <vilesyn@gmail.com>
2020-04-29 18:30:43 -0500

    switch to material-ui global styling

--- app/components/app.js
+++ app/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { createGlobalStyle } from 'styled-components'
+import { withStyles } from '@material-ui/core/styles'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import Main from './main'
 
@@ -29,48 +29,45 @@ export default class App extends Component {
   }
 
   render() {
+    const GlobalCss = withStyles({
+      '@global': {
+        'html, body, #app': {
+          boxSizing: 'border-box',
+          margin: 0,
+          padding: 0,
+          fontFamily: '\'Press Start 2P\', Audiowide',
+          color: 'white',
+          height: '100%'
+        },
+        'div': {
+          padding: '0.5 1rem'
+        },
+        'ul::-webkit-scrollbar': {
+          width: '0 !important'
+        },
+        '.vex-content': {
+          background: `${vars.folderBackgroundDark} !important`,
+          color: `${vars.systemOffwhite} !important`,
+          '&.vex-dialog-button': {
+            boxShadow: 'inset 0 3px #111 !important'
+          }
+        },
+        '.vex-dialog-button.vex-last': {
+          background: `${vars.folderBackground} !important`
+        },
+        '.vex-dialog-button.vex-first': {
+          '&:hover': {
+            backgroundColor: `${vars.folderBackgroundDark} !important`
+          }
+        }
+      }
+    })(() => null);
+
     return (
       <MuiThemeProvider theme={theme}>
+        <GlobalCss />
         <Main />
-        <Global />
       </MuiThemeProvider>
     )
   }
 }
-
-const Global = createGlobalStyle`
-  html, body, #app {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    font-family: 'Press Start 2P', 'Audiowide';
-    color: 'white';
-    height: 100%;
-  }
-
-  div {
-    padding: 0.5 1rem;
-  }
-
-  ul::-webkit-scrollbar { width: 0 !important }
-
-  .vex-content {
-    background: ${vars.folderBackgroundDark} !important;
-    color: ${vars.systemOffwhite} !important;
-
-    &.vex-dialog-button {
-      box-shadow: inset 0 3px #111 !important;
-    }
-  }
-  
-  .vex-dialog-button.vex-last {
-    background: ${vars.folderBackground} !important;
-  }
-
-  .vex-dialog-button.vex-first {
-    &:hover {
-      background-color: ${vars.folderBackgroundDark} !important;
-    }
-  }
-`
-
