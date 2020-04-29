998958d8a9d319dc706ae02a4117e89b294d0d51
Dennis Brown <vilesyn@gmail.com>
2020-04-29 17:16:12 -0500

    cleanup confirm component

--- app/components/visual/confirm.js
+++ app/components/visual/confirm.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import ContainedButton from './ContainedButton'
 
 const Confirm = ({ copy, copySelect, copyFolder }) => (
-  <>
-    {copySelect && copy ?
-      <ContainedButton title="copy selected settings folder" name="Copy" action={copyFolder} />
-    :
-      null
-    }
-  </>
+  copySelect && copy ?
+    <ContainedButton title="copy selected settings folder" name="Copy" action={copyFolder} />
+  :
+    null
 )
 
 Confirm.propTypes = {
-  copyFolder: PropTypes.func,
-  copySelect: PropTypes.string,
-  copy: PropTypes.bool
+  copyFolder: PropTypes.func.isRequired,
+  copySelect: PropTypes.string.isRequired,
+  copy: PropTypes.bool.isRequired
 }
 
 export default Confirm
