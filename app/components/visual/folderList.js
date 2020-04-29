64674f48a41f570fbb620da6b255c4ad25d2fe38
Dennis Brown <vilesyn@gmail.com>
2020-04-29 17:41:14 -0500

    change folderlist to dumb component

--- app/components/visual/folderList.js
+++ app/components/visual/folderList.js
@@ -1,73 +1,83 @@
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import { makeStyles } from '@material-ui/core/styles'
 
 import ContainedButton from './ContainedButton'
 import Folder from './folder'
 import * as vars from '../../style/variables'
 
-const FolderList = ({ folders, selectFolder, selected, cancel, ...props }) => (
-  <Fragment>
-    <Folders>
-      {props.folderMissing ?
-        <List>
-          <Message>Blender Data Missing</Message>
-        </List>
-      :
-        <List>
-          {folders.map(folder => (
-            <ListItem key={folder}>
-              <Folder name={folder} select={selectFolder} selected={selected} />
-            </ListItem>
-          ))}
-        </List>
-      }
-    </Folders>
-    {cancel ?
-      <ButtonContainer>
-        {props.children}
-        <ContainedButton title="cancel" color="secondary" action={() => cancel()} name="Cancel" />
-      </ButtonContainer>
-    : null
+const FolderList = props => {
+  const {
+    cancel,
+    children,
+    folderMissing,
+    folders,
+    selectFolder,
+    selected
+  } = props
+
+  const useStyles = makeStyles({
+    button: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      marginBottom: '48px'
+    },
+    folder: {
+      display: 'flex',
+      flexDirection: 'column',
+      borderRadius: '5px',
+      backgroundColor: vars.folderBackgroundDark,
+      boxShadow: 'inset 0px 2px 2px 1px hsla(0, 0%, 0%, 0.1)',
+      height: '100%',
+      margin: '24px'
+    },
+    list: {
+      listStyle: 'none',
+      padding: '2px 8px',
+      overflowY: 'scroll'
+    },
+    listItem: {
+      textAlign: 'left'
+    },
+    message: {
+      color: vars.offWhite
     }
-  </Fragment>
-)
+  })
 
-const Message = styled.div`
-  color: ${vars.offWhite};
-`
+  const classes = useStyles()
 
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  margin-bottom: 48px;
-`
-
-const Folders = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-radius: 5px;
-  background-color: ${vars.folderBackgroundDark};
-  box-shadow: inset 0px 2px 2px 1px hsla(0, 0%, 0%, 0.1);
-  height: 100%;
-  margin: 24px;
-`
-
-const List = styled.ul`
-  list-style: none;
-  padding: 0 8px;
-  overflow-y: scroll;
-`
-
-const ListItem = styled.li`
-  text-align: left;
-`
+  return (
+    <>
+      <div className={classes.folder}>
+        {folderMissing ?
+          <ul className={classes.list}>
+            <div className={classes.message}>Blender Data Missing</div>
+          </ul>
+        :
+          <div className={classes.list}>
+            {folders.map(folder => (
+              <li className={classes.listeItem} key={folder}>
+                <Folder name={folder} select={selectFolder} selected={selected} />
+              </li>
+            ))}
+          </div>}
+      </div>
+      {cancel ?
+        <div className={classes.button}>
+          {children}
+          <ContainedButton title="cancel" color="secondary" action={() => cancel()} name="Cancel" />
+        </div>
+      : null}
+    </>
+  )
+}
 
 export default FolderList
 
 FolderList.propTypes = {
-  folders: PropTypes.array,
+  children: PropTypes.node,
+  folders: PropTypes.arrayOf(PropTypes.string).isRequired,
   select: PropTypes.func,
   selected: PropTypes.string,
   cancel: PropTypes.func
